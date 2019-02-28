0cc80899c271abdd8ceaf7e415269ee43f10f4aa
Emma Englishby <emma@englishby.co>
2019-02-28 15:17:45 +0100

    Add separate work list

--- app.js
+++ app.js
@@ -6,38 +6,53 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let newToDos = [];
+let workToDos = [];
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
+/******* GET REQUESTS *******/
 app.get("/", function(req, res){
-let today = new Date();
-let options = {
-  weekday: "long",
-  month: "long",
-  day: "2-digit"
-};
-let day = today.toLocaleDateString("en-US", options);
+  let today = new Date();
+  let options = {
+    weekday: "long",
+    month: "long",
+    day: "2-digit"
+  };
+  let day = today.toLocaleDateString("en-US", options);
 
-/*  let todayDate = new Date();
-  let today = todayDate.getDay();
-  let weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  let day = {
-    day: weekdays[today]
-  } */
+  res.render("list", {listTitle: day, newToDos: newToDos});
+});
 
-  res.render("list", {day: day, newToDos: newToDos});
+app.get("/work", function(req,res){
+  res.render("list", {listTitle: "Work List", newToDos: workToDos});
 });
 
 app.post("/", function(req, res){
+  console.log(req.body);
+
+  let newToDo = req.body.newToDo; //var containing whatever is typed by user in form
+
+  if (req.body.list === "Work") { //
+    workToDos.push(newToDo);
+    res.redirect("/work");
+  } else {
+    newToDos.push(newToDo);
+    res.redirect("/");
+  }
+});
+
+
+
+/*app.post("/work", function(req,res){
   let newToDo = req.body.newToDo;
 
   newToDos.push(newToDo);
 
-  res.redirect("/");
-});
+  res.redirect("/work");
+}); */
 
 app.listen(3000, function(){
   console.log("Server started on port 3000.");
